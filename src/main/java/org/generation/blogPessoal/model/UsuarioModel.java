5618b63561d7ca3290acea811a7391a7edcad338
Aline Beltrao <alinebelltrao@gmail.com>
2022-08-18 15:51:26 -0300

    Deploy do Projeto Blog Pessoal

--- src/main/java/org/generation/blogPessoal/model/UsuarioModel.java
+++ src/main/java/org/generation/blogPessoal/model/UsuarioModel.java
@@ -16,6 +16,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name="tb_usuarios")
 public class UsuarioModel {
@@ -27,6 +29,7 @@ public class UsuarioModel {
 	@NotNull(message = "O atributo nome é obrigatório!")
 	private String nome;
 	
+	@Schema(example = "email@email.com.br")
 	@NotNull(message= "O atributo usuario é obrigatório!")
 	@Email(message="O atributo e-mail é obrigatório")
 	private String usuario;
--- src/main/java/org/generation/blogPessoal/security/UserDetailsServiceImpl.java
+++ src/main/java/org/generation/blogPessoal/security/UserDetailsServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.generation.blogPessoal.model.UsuarioModel;
 import org.generation.blogPessoal.repository.UsuarioRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
